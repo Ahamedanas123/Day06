363abd30e4242c82ac62d00432dbc3dfef006c08
Ahamedanas123 <121920106+Ahamedanas123@users.noreply.github.com>
2023-01-28 13:35:26 +0530

    done
    done

---
+++ script.js
@@ -0,0 +1,52 @@
+// odd numbers in anonyums function
+
+let arr=[15,18,17,19,5,7,6,4,3,8,9,1];
+let even=arr.filter(n => n%2)
+console.log(even);
+
+//add array
+
+const myarr = [5, 15, 45];
+
+const sum = myarr.reduce((accumulator, value) => {
+  return accumulator + value;
+});
+
+console.log(sum);
+
+// prime number
+
+var numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numArray = numArray.filter((number) => {
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+});
+
+console.log(numArray);
+
+//palindrome
+
+let str = "ana";
+let strReverse = str.split("").reverse().join("");
+if (str === strReverse) {
+  console.log("The string is a palindrome");
+} else {
+  console.log("NOT a palindrome");
+}
+
+//duplicate
+
+var arr1 = ["apple", "mango", "apple",
+            "orange", "mango", "mango"];
+ 
+    function removeDuplicates(arr1) {
+        return arr1.filter((item,index)=> arr1.indexOf(item) === index);
+
+    }
+        
+        console.log(removeDuplicates(arr1));
+
+       
